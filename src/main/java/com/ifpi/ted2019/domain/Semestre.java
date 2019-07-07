8f579cd439d5c1514126ada284517d70361d65bf
Paulo2301 <paulinho.ricardo23@gmail.com>
2019-07-07 16:47:14 -0300

    Primeira classe de dominio

---
+++ src/main/java/com/ifpi/ted2019/domain/Semestre.java
@@ -0,0 +1,78 @@
+package com.ifpi.ted2019.domain;
+
+import java.io.Serializable;
+
+public class Semestre implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Integer Id;
+	private Integer ano;
+	private Integer periodo;
+	
+	public Semestre() {
+		super();
+	}
+
+	public Semestre(Integer id, Integer ano, Integer periodo) {
+		super();
+		Id = id;
+		this.ano = ano;
+		this.periodo = periodo;
+	}
+
+	public Integer getId() {
+		return Id;
+	}
+
+	public void setId(Integer id) {
+		Id = id;
+	}
+
+	public Integer getAno() {
+		return ano;
+	}
+
+	public void setAno(Integer ano) {
+		this.ano = ano;
+	}
+
+	public Integer getPeriodo() {
+		return periodo;
+	}
+
+	public void setPeriodo(Integer periodo) {
+		this.periodo = periodo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Semestre other = (Semestre) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		return true;
+	}
+	
+	
+	
+
+}
--- src/main/java/com/ifpi/ted2019/resources/SemestreResources.java
+++ src/main/java/com/ifpi/ted2019/resources/SemestreResources.java
@@ -1,14 +1,23 @@
 package com.ifpi.ted2019.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ifpi.ted2019.domain.Semestre;
+
 @RestController
 @RequestMapping(value = "/semestres")
 public class SemestreResources {
+	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar(){
-		return "Rest Funcionando";
+	public List<Semestre> listar(){
+		List<Semestre> semestre= new ArrayList<Semestre>();
+		semestre.add(new Semestre(1, 2019, 2));
+		semestre.add(new Semestre(2, 2017, 1));
+		return semestre;
 	}
 }
