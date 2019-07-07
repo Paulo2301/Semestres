0909a5182f3c0144e90d9cc158a9f4456c361087
Paulo2301 <paulinho.ricardo23@gmail.com>
2019-07-07 16:17:42 -0300

    Primeiro commit

---
+++ src/main/java/com/ifpi/ted2019/SemestrePaulo1Application.java
@@ -0,0 +1,13 @@
+package com.ifpi.ted2019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemestrePaulo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemestrePaulo1Application.class, args);
+	}
+
+}
